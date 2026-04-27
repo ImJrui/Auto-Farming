aa91b54a0ab96b3934d7eaf6797b4bc4e6264df8
ImJrui <huangjianrui1997@163.com>
2026-04-27 11:25:44 +0800

    长按热键开启自动照顾

--- modmain.lua
+++ modmain.lua
@@ -35,49 +35,140 @@ local CFG_SHOW_STATE = GetModConfigData("CFG_SHOW_STATE")
 local CFG_STATUS_HEIGHT = GetModConfigData("CFG_STATUS_HEIGHT")
 
 AddPlayerPostInit(function(inst)
-    if not TheWorld.ismastersim then
-        inst:AddComponent("autofarm")
-        inst:AddComponent("autoplant")
-        inst.components.autofarm:SetShowStatus(CFG_SHOW_STATE)
-        inst.components.autofarm:SetStatusHeight(CFG_STATUS_HEIGHT)
-        inst.components.autoplant:SetShowStatus(CFG_SHOW_STATE)
-        inst.components.autoplant:SetStatusHeight(CFG_STATUS_HEIGHT)
-    end
+    inst:AddComponent("autofarm")
+    inst:AddComponent("autoplant")
+    inst.components.autofarm:SetShowStatus(CFG_SHOW_STATE)
+    inst.components.autofarm:SetStatusHeight(CFG_STATUS_HEIGHT)
+    inst.components.autoplant:SetShowStatus(CFG_SHOW_STATE)
+    inst.components.autoplant:SetStatusHeight(CFG_STATUS_HEIGHT)
 end)
 
 --------------------------------------------------
 -- OnHotKey
--- Hotkey = ON→OFF, OFF+Ctrl→continue, OFF→open menu
+-- Hotkey = ON->OFF, OFF long press->continue, OFF short press->open menu
 --------------------------------------------------
-local function OnHotKey()
+local HOTKEY_LONG_PRESS_TIME = 0.5
+local hotkey_task = nil
+local hotkey_is_down = false
+local hotkey_pending_short_press = false
+local hotkey_long_pressed = false
+
+local function IsHotKeyBlocked()
     if not ThePlayer or not ThePlayer.HUD or ThePlayer.HUD:HasInputFocus() then
-        return
+        return true
     end
 
+    return false
+end
+
+local function GetAutoComponents()
     local autofarm = ThePlayer.components.autofarm
     local autoplant = ThePlayer.components.autoplant
-    local is_running = (autofarm and autofarm:IsEnabled()) or (autoplant and autoplant:IsEnabled())
 
-    if is_running then
-        if autofarm then autofarm:Disable() end
-        if autoplant then autoplant:Disable() end
-        ThePlayer.HUD:HideStatusDisplayer()
-        ThePlayer.HUD:CloseComboScreen()
-        ThePlayer.HUD:CloseComboConfirmScreen()
-    elseif TheInput:IsKeyDown(KEY_CTRL) then
-        if autoplant and autoplant:HasUnfinishedTargets() then
-            autoplant:Enable()
-        else
-            autofarm:Enable()
-        end
-    else
-        if autoplant then
-            autoplant:OpenComboScreen()
-        end
+    return autofarm, autoplant
+end
+
+local function IsAutoRunning(autofarm, autoplant)
+    return (autofarm and autofarm:IsEnabled()) or (autoplant and autoplant:IsEnabled())
+end
+
+local function StopAuto(autofarm, autoplant)
+    if autofarm then autofarm:Disable() end
+    if autoplant then autoplant:Disable() end
+    ThePlayer.HUD:HideStatusDisplayer()
+    ThePlayer.HUD:CloseComboScreen()
+    ThePlayer.HUD:CloseComboConfirmScreen()
+end
+
+local function ContinueAuto(autofarm, autoplant)
+    if autoplant and autoplant:HasUnfinishedTargets() then
+        autoplant:Enable()
+    elseif autofarm then
+        autofarm:Enable()
     end
 end
 
-TheInput:AddKeyDownHandler(GetModConfigData("CFG_KEY"), OnHotKey)
+local function OpenAutoMenu(autoplant)
+    if autoplant then
+        autoplant:OpenComboScreen()
+    end
+end
+
+local function CancelHotKeyTask()
+    if hotkey_task then
+        hotkey_task:Cancel()
+        hotkey_task = nil
+    end
+end
+
+local function OnHotKeyLongPress()
+    hotkey_task = nil
+    if not hotkey_pending_short_press then
+        return
+    end
+
+    hotkey_long_pressed = true
+
+    if IsHotKeyBlocked() then
+        return
+    end
+
+    local autofarm, autoplant = GetAutoComponents()
+    if not IsAutoRunning(autofarm, autoplant) then
+        ContinueAuto(autofarm, autoplant)
+    end
+end
+
+local function OnHotKeyDown()
+    if hotkey_is_down then
+        return
+    end
+
+    if IsHotKeyBlocked() then
+        return
+    end
+
+    hotkey_is_down = true
+
+    local autofarm, autoplant = GetAutoComponents()
+    if IsAutoRunning(autofarm, autoplant) then
+        hotkey_pending_short_press = false
+        hotkey_long_pressed = false
+        CancelHotKeyTask()
+        StopAuto(autofarm, autoplant)
+        return
+    end
+
+    if hotkey_pending_short_press then
+        return
+    end
+
+    hotkey_pending_short_press = true
+    hotkey_long_pressed = false
+    CancelHotKeyTask()
+    hotkey_task = ThePlayer:DoTaskInTime(HOTKEY_LONG_PRESS_TIME, OnHotKeyLongPress)
+end
+
+local function OnHotKeyUp()
+    if not hotkey_is_down and not hotkey_pending_short_press then
+        return
+    end
+
+    local should_open_menu = hotkey_pending_short_press and not hotkey_long_pressed
+    hotkey_is_down = false
+    hotkey_pending_short_press = false
+    hotkey_long_pressed = false
+    CancelHotKeyTask()
+
+    if should_open_menu and not IsHotKeyBlocked() then
+        local _, autoplant = GetAutoComponents()
+        OpenAutoMenu(autoplant)
+    end
+end
+
+local CFG_KEY = GetModConfigData("CFG_KEY")
+TheInput:AddKeyDownHandler(CFG_KEY, OnHotKeyDown)
+TheInput:AddKeyUpHandler(CFG_KEY, OnHotKeyUp)
 
 --------------------------------------------------
 -- ComboScreen Postinit
